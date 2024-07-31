f98261a240ef21a7991dabbe0eb442406905812c
Deivis Matheus <dmatheusccs@gmail.com>
2024-07-31 10:26:00 -0400

    mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-9c32958f-493b-47ce-b28a-fc0692d03fc0.containerhub.tripleten-services.com/"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+USERS_TABLE_PATH = "/api/v1/users/"  # Ajusta esto según la ruta real para obtener la tabla de usuarios
